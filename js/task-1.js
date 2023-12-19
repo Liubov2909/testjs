ca4b6435778cfe0e883b84047a3a1ef6dbe965b1
Liubov2909 <ostapcuk2909@gmail.com>
2023-12-19 18:26:42 +0200

    test

--- js/task-1.js
+++ js/task-1.js
@@ -1,1 +1,12 @@
-console.log(_);
+const form = document.querySelector(".feedback-form");
+const localStorageKey = "goit-example-message";
+
+form.addEventListener("input", (evt) => {
+  localStorage.setItem(localStorageKey, evt.target.value);
+});
+
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+	console.log(evt.target.elements.message.value);
+  form.reset();
+});
