bb33d589564604c2e72ea819fa287b9cd20f435b
Liubov2909 <ostapcuk2909@gmail.com>
2023-12-20 15:53:17 +0200

    dz9 js corrected

--- js/task-1.js
+++ js/task-1.js
@@ -1,12 +1,17 @@
-const form = document.querySelector(".feedback-form");
-const localStorageKey = "goit-example-message";
+// const form = document.querySelector(".feedback-form");
+// const localStorageKey = "goit-example-message";
 
-form.addEventListener("input", (evt) => {
-  localStorage.setItem(localStorageKey, evt.target.value);
-});
+// form.addEventListener("input", (evt) => {
+//   localStorage.setItem(localStorageKey, evt.target.value);
+// });
 
-form.addEventListener("submit", (evt) => {
-  evt.preventDefault();
-	console.log(evt.target.elements.message.value);
-  form.reset();
-});
+// form.addEventListener("submit", (evt) => {
+//   evt.preventDefault();
+// 	console.log(evt.target.elements.message.value);
+//   form.reset();
+// });
+
+const date = new Date("2030-03-16");
+
+console.log(date);
+
